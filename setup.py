4735a29779d9edbfa2c47a9dd6ca3de64c31a321
Guillaume Train <g.train@live.fr>
2024-10-17 16:02:48 +0200

    add PiPy auto push repo

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
+import os
 from setuptools import setup, find_packages
 
-
 def parse_requirements(filename):
-    with open(filename, 'r') as f:
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    with open(filepath, 'r') as f:
         return [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
-    name='PyDataCore',  # Nom du package
-    version='1.0.0',  # Version de ta bibliothèque
-    author='Guillaume Train',  # Auteur
-    author_email='g.train@live.fr',  # Email de l'auteur
+    name='PyDataCore',
+    version='1.0.0',
+    author='Guillaume Train',
+    author_email='g.train@live.fr',
     description='A data library for handling temporal, frequency signals, and data pools.',
-    long_description=open('README.md').read(),  # Longue description du projet
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/GuillaumeTrain/PyDataCore',  # Lien vers ton repo GitHub
+    url='https://github.com/GuillaumeTrain/PyDataCore',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    include_package_data=True,  # Inclut des fichiers de données dans la distribution
-    classifiers=[  # Catégories de la bibliothèque
+    include_package_data=True,
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.10',  # Version minimum de Python
+    python_requires='>=3.10',
     install_requires=parse_requirements('requirements.txt'),
 )
