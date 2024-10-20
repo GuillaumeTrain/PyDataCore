f2364b9a937bc9981da1baa5778922481ab5a595
Guillaume Train <g.train@live.fr>
2024-10-20 15:54:27 +0200

    Initial commit for the DataPool enhancements with advanced chunk management and memory optimizations
    - Added detailed sample type handling (`float32`, `float64`, `int32`, `int64`, `str`)
    - Introduced `ChunkableMixin` and `FileRamMixin` to support large data handling by storing and retrieving data in chunks (RAM and file-based storage)
    - Implemented chunked data processing with `store_data_from_data_generator`, `read_chunked_data`, and overlapped chunking (`read_overlapped_chunked_data`)
    - Enhanced `DataPool` to better manage relationships between sources, data, and subscribers
    - Added comprehensive test cases for `DataPool` and `Data` classes to validate storage methods, memory management, and handling of large datasets
    - Integrated memory leak detection using `tracemalloc`
    - Tested data types include `TEMPORAL_SIGNAL`, `FREQ_SIGNAL`, `FFTS`, `INTS`, `STR`, and more, with support for both in-RAM and file-based storage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyDataCore',
-    version='1.0.1',
+    version='1.0.2',
     author='Guillaume Train',
     author_email='g.train@live.fr',
     description='A data library for handling temporal, frequency signals, and data pools.',
